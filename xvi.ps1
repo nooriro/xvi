0b1d79b2696ecccbeb719ee8ce16476444e69f34
nooriro <noorirogit@gmail.com>
2020-03-23 14:05:10 +0900

    Support old versions of Windows that doesn't have built-in bsdtar
    Built-in bsdtar is only available on Windows 10 19H1
    (Version 1903, Build 18362) or higher.[1]
    [1] Insider Build: Build 17063 or higher
    
    Sadly, a lot of people even using Windows 10 don't update to
    the latest version and use an older version of Windows.
    In this case, they can install 7-Zip and use it to extract
    vendor.img file manually. But this can be a hassle.
    
    So for those who use an old version of Windows that
    doesn't have built-in bsdtar, this commit adds a feature
    to automatically download 7za.exe (version 19.00, x86 binary)
    and use it to decompress tgz files.
    
    Of course, when running on Windows 10 Build 17063 or higher,
    7za.exe is not download and the built-in bsdtar is still used.

--- xvi.ps1
+++ xvi.ps1
@@ -4,7 +4,6 @@
 # * Extracts only vendor image from Google's driver binaries tgz [1]
 #   [1] https://developers.google.com/android/drivers
 # * The vendor image will be renamed to 'vendor-userdebug.img'
-# * Requires Windows 10 Build 17063 or higher to run tar
 # ------------------------------------------------------------------------------
 # Usage: (1) Download a 'vendor image' tgz into working directory
 #        (2) Place this script in the same directory
@@ -13,10 +12,10 @@
 # ------------------------------------------------------------------------------
 
 
+$nativeTar = $true
 if ( $PSEdition -ne "Core" -or $IsWindows ) {
   if ( [Environment]::OSVersion.Version.Build -lt 17063 ) {
-    "! This script requires Windows 10 Build 17063 or higher"
-    exit 1
+    $nativeTar = $false
   }
 }
 
@@ -25,6 +24,12 @@ Set-Variable -Option Constant  -Name LF                  -Value ([Byte]10)
 Set-Variable -Option Constant `
              -Name TGZ_FILENAME_REGEX `
              -Value "google_devices-([a-z]+)-([a-z0-9\.]+)-([0-9a-f]{8})\.tgz"
+if ( -Not $nativeTar ) {
+  Set-Variable -Option Constant `
+               -Name SEVENZIPA_URL `
+               -Value "https://github.com/nooriro/xvi/raw/0360be2fe052630156e28495650d383d68c0c6d8/7za.exe"
+  Set-Variable -Option Constant  -Name SEVENZIPA_FILENAME  -Value "7za.exe"
+}
 
 $tgz = Get-ChildItem *.tgz -File | 
   Where-Object { $_.Name -match $TGZ_FILENAME_REGEX } |
@@ -35,7 +40,7 @@ if ( ! $tgz ) {
   "! No vendor image tgz file in current directory"
   "  Download a 'vendor image' tgz on https://developers.google.com/android/drivers"
   "  and place it in the current directory, and run this script again."
-  exit 2
+  exit 1
 }
 
 $tgzName      = $tgz.Name
@@ -49,13 +54,30 @@ $tgzHash      = $tgz.Name -replace $TGZ_FILENAME_REGEX, '$3'
 if ( Test-Path "vendor-userdebug.img" ) {
   "! 'vendor-userdebug.img' exists in current directory"
   "  Remove, rename, or move that file and run this script again."
-  exit 3
+  exit 2
 }
 
 "- Extracing file(s) from vendor image tgz"
 $dir = New-Guid
 $null = New-Item $dir -Type Directory
-tar xvzf "$tgzName" -C "$dir" 2> $null
+if ( $nativeTar ) {
+  tar xvzf "$tgzName" -C "$dir" 2> $null
+} else {
+  try {
+    # "3 ways to download files with PowerShell"
+    # https://blog.jourdant.me/post/3-ways-to-download-files-with-powershell
+    $wc = [Net.WebClient]::new()
+    $wc.DownloadFile( $SEVENZIPA_URL, "${pwd}\${dir}\${SEVENZIPA_FILENAME}" )
+  } catch {
+    "! An error occured while downloading ${SEVENZIPA_FILENAME}"
+    $_
+    Remove-Item $dir -Recurse -Force
+    exit 3
+  }
+  # "Programmatically extract tar.gz in a single step (on Windows with 7-Zip)"
+  # https://stackoverflow.com/a/14699663
+  & cmd "/c ${dir}\7za x ""${tgzName}"" -so | ${dir}\7za x -aoa -si -ttar -o""${dir}"" > NUL"
+}
 
 "- Skipping first $($PAYLOAD_START_LINE - 1) lines of  [extract-google_devices-${tgzDevice}.sh]"
 Set-Location $dir
@@ -72,7 +94,11 @@ $fs.Write( $bytes, $p, $bytes.Length - $p )
 $fs.Close()
 
 "- Extracing file(s) from payload data tgz"
-tar xvzf payload.tgz 2> $null
+if ( $nativeTar ) {
+  tar xvzf payload.tgz 2> $null
+} else {
+  & cmd "/c 7za x payload.tgz -so | 7za x -aoa -si -ttar > NUL"
+}
 
 "- Moving vendor.img to         [vendor-userdebug.img] in current directory"
 Move-Item "vendor/google_devices/${tgzDevice}/proprietary/vendor.img" ../vendor-userdebug.img
